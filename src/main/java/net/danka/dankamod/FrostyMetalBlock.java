d90cb6823742ec00a3e8698251921f72a03dc3ef
DaNkaCode <dankplay.5461@gmail.com>
2025-03-04 13:06:53 +0300

    Create FrostyMetalBlock.java

---
+++ src/main/java/net/danka/dankamod/FrostyMetalBlock.java
@@ -0,0 +1,15 @@
+package com.example.mod;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.sound.BlockSoundGroup;
+
+
+public class FrostyMetalBlock extends Block {
+    public FrostyMetalBlock() {
+        super(Settings.of(Material.STONE
+                .strength(2.0f, 3.0f)
+                .sounds(BlockSoundGroup.STONE) 
+                .requiresTool());
+    }
+}
